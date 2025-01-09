8cff8c6226655260ce58edd486de043f41cdcfe8
Abednego7 <isaacestampida7@gmail.com>
2025-01-09 13:01:26 -0300

    Database and Necessary Configurations

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-=ge-77pu@e+#ob60&$e*@^!@_$xr1o5g-)-ea=#3_j5c^)s*^%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]  # la cadena tiene un 'nombre' cualquiera
+ALLOWED_HOSTS = []  # la cadena tiene un 'nombre' cualquiera -> ".vercel.app"
 
 
 # Application definition
