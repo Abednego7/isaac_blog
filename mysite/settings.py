69ee5e3ae16dcd0eff1ba9400abb3c2594b43438
Abednego7 <isaacestampida7@gmail.com>
2025-01-09 15:08:43 -0300

    Some security changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=ge-77pu@e+#ob60&$e*@^!@_$xr1o5g-)-ea=#3_j5c^)s*^%"
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("IS_DEVELOPMENT", True)
 
-ALLOWED_HOSTS = []  # la cadena tiene un 'nombre' cualquiera -> ".vercel.app"
+ALLOWED_HOSTS = [getenv("APP_HOST")]
 
 
 # Application definition
@@ -85,8 +85,12 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": getenv("DATABASE_NAME"),
+        "USER": getenv("DATABASE_USER"),
+        "PASSWORD": getenv("DATABASE_PASSWORD"),
+        "HOST": getenv("DATABASE_HOST"),
+        "PORT": getenv("DATABASE_PORT"),
     }
 }
 
