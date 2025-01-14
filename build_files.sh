631e451778e4916029170453eae4b77f42d97a79
Abednego7 <isaacestampida7@gmail.com>
2025-01-14 17:48:05 -0300

    build_files

--- build_files.sh
+++ build_files.sh
@@ -1,1 +1,8 @@
+# Install Python dependencies
 pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Create uploads directory if it doesn't exist
+mkdir -p uploads/posts
